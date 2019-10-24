ac421563eec826902a2bb59366ea266a80e86065
Kasia Zukowska <kzukowska@hotmail.com>
2019-10-24 13:02:28 +0200

    collections and art

--- src/Image/Image.js
+++ src/Image/Image.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { render } from 'react-dom'
 import './image.css'
 
 class Image extends Component {
@@ -7,33 +6,72 @@ class Image extends Component {
   constructor() {
     super()
     this.state = {
-      image: '',
-      imgID: 'f1bbfd0b-7436-4cb2-ab12-04f6bb79ec89.jpg'
+      image: 'https://iip.smk.dk/iiif/jp2/kks1982-156.tif.reconstructed.tif.jp2/full/!1600,/0/default.jpg',
+      imgID: 'kks1982-156.tif.reconstructed.tif.jp2',
+      collectionIDs: []
     }
   }
 
-  componentWillMount() {
+  componentDidMount() {
+    this.getIDs()
+    console.log('state ', this.state)
+    console.log(this.state.collectionIDs[0])
+    // this.getArt('kks1982-156')
+    this.getArt('KKS1965-14')
     this.getImage(this.state.imgID)
+  }
+
+  getIDs = () => {
+    var xhr = new XMLHttpRequest()
+    xhr.responseType = 'json';
+    let jsonResponse
+    xhr.addEventListener('load', () => {
+
+      jsonResponse = xhr.response
+      console.log(jsonResponse.objectIDs)
+      this.setState({
+        collectionIDs: jsonResponse.objectIDs
+      })
+    })
+
+    xhr.open('GET', 'https://api.smk.dk/api/v1/art/all_ids')
+    xhr.send()
+  }
+
+  getArt = (collectionID) => {
+    var _this = this;
+    var xhr = new XMLHttpRequest()
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', () => {
+      console.log('getArt ', xhr.response)
+      _this.setState({
+        image: xhr.response.thumbnailUrl
+      })
+    })
+
+    xhr.open('GET', 'https://api.smk.dk/api/v1/art/?object_number=' + collectionID + '&output=JSON-LD&lang=en')
+    xhr.send()
   }
 
   getImage = (imgID) => {
     var xhr = new XMLHttpRequest()
 
     xhr.addEventListener('load', () => {
-      console.log(xhr.responseURL)
       this.setState({
         image: xhr.responseURL
       })
     })
 
-    xhr.open('GET', 'https://api.smk.dk/api/v1/thumbnail/' + imgID)
+    // xhr.open('GET', 'https://api.smk.dk/api/v1/thumbnail/' + imgID)
+    xhr.open('GET', this.state.image)
     xhr.send()
   }
 
   render() {
     return (
-      <div>
-        <img src={this.state.image} width="300" />
+      <div className="center">
+        <img src={this.state.image} width="400" alt="something" className="thumbnail"/>
       </div>
     )
   }
