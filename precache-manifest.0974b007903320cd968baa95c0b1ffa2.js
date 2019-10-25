f68b5067e545f4c13531488c0df446481ebcdc6b
Kasia Zukowska <kzukowska@hotmail.com>
2019-10-25 18:00:01 +0200

    Updates

---
+++ precache-manifest.0974b007903320cd968baa95c0b1ffa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ec7fcf6d5171d20999b52c4d304c99",
+    "url": "/smk-API/index.html"
+  },
+  {
+    "revision": "73111de974158a03b58a",
+    "url": "/smk-API/static/css/main.562ff59e.chunk.css"
+  },
+  {
+    "revision": "819ae9bcf8c9cb782013",
+    "url": "/smk-API/static/js/2.49716f86.chunk.js"
+  },
+  {
+    "revision": "73111de974158a03b58a",
+    "url": "/smk-API/static/js/main.5983fbbf.chunk.js"
+  },
+  {
+    "revision": "31de1d5bc72053d9ae22",
+    "url": "/smk-API/static/js/runtime-main.b947fbf4.js"
+  }
+]);
