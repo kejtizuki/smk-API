25f52a0d71a3a9a8e93e91fe3aac282e142c9611
Kasia Zukowska <kzukowska@hotmail.com>
2019-10-25 17:50:54 +0200

    instructions

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-Try to follow instructions below, otherwise you can fork this repo, run the code and understand it.
+Try to follow instructions below, otherwise you can fork this repo, run the code and go through it.
 
-1. Go to https://github.com/facebook/create-react-app and create a new app.
-2. Create Image component.
+## Instructions
+1. Install npm.
+2. Go to https://github.com/facebook/create-react-app and create a new app.
+3. In src create Image folder that will contain Image.js and image.css file.
+4. In Image.js create new Image component.
+5. Image component should contain the following functions:
+
+  ```bash
+    getIDs() - should GET the array of all IDs
+    getArt(collectionID) - takes collectionID as parameter and GETs image url
+    getArtInfo(collectionID) - takes collectionID as parameter and GETs artist name
+  ```
--- src/Image/Image.js
+++ src/Image/Image.js
@@ -6,72 +6,100 @@ class Image extends Component {
   constructor() {
     super()
     this.state = {
-      image: 'https://iip.smk.dk/iiif/jp2/kks1982-156.tif.reconstructed.tif.jp2/full/!1600,/0/default.jpg',
-      imgID: 'kks1982-156.tif.reconstructed.tif.jp2',
-      collectionIDs: []
+      collectionIDs: [],
+      id: 'kks1982-156'
+      // Try other ids, for example 'kks1982-156',
     }
   }
 
   componentDidMount() {
     this.getIDs()
-    console.log('state ', this.state)
-    console.log(this.state.collectionIDs[0])
-    // this.getArt('kks1982-156')
-    this.getArt('KKS1965-14')
-    this.getImage(this.state.imgID)
+
+    this.getArt(this.state.id)
+    this.getArtInfo(this.state.id)
+  }
+
+  componentDidUpdate(prevState) {
+    if (prevState.collectionIDs !== this.state.collectionIDs) {
+      console.log('print collection IDs ', this.state.collectionIDs)
+    }
   }
 
   getIDs = () => {
-    var xhr = new XMLHttpRequest()
-    xhr.responseType = 'json';
-    let jsonResponse
-    xhr.addEventListener('load', () => {
 
-      jsonResponse = xhr.response
-      console.log(jsonResponse.objectIDs)
-      this.setState({
-        collectionIDs: jsonResponse.objectIDs
-      })
-    })
+    fetch('https://api.smk.dk/api/v1/art/all_ids')
+     .then(response => response.json())
+     .then(data => this.setState({ collectionIDs: data.objectIDs }));
 
-    xhr.open('GET', 'https://api.smk.dk/api/v1/art/all_ids')
-    xhr.send()
+
+    //Another way of geting IDs array
+
+    // var xhr = new XMLHttpRequest()
+    // xhr.responseType = 'json';
+    // let jsonResponse
+    // xhr.addEventListener('load', () => {
+    //
+    //   jsonResponse = xhr.response
+    //   console.log(jsonResponse.objectIDs)
+    //   this.setState({
+    //     collectionIDs: jsonResponse.objectIDs
+    //   })
+    // })
+    //
+    // xhr.open('GET', 'https://api.smk.dk/api/v1/art/all_ids')
+    // xhr.send()
   }
 
+  // Get image using collectionID
   getArt = (collectionID) => {
-    var _this = this;
-    var xhr = new XMLHttpRequest()
-    xhr.responseType = 'json';
 
-    xhr.addEventListener('load', () => {
-      console.log('getArt ', xhr.response)
-      _this.setState({
-        image: xhr.response.thumbnailUrl
-      })
-    })
+    fetch('https://api.smk.dk/api/v1/art/?object_number=' + collectionID + '&output=JSON-LD&lang=en')
+     .then(response => response.json())
+     .then(data => {console.log('dataaa', data); this.setState({ image: data.thumbnailUrl })});
 
-    xhr.open('GET', 'https://api.smk.dk/api/v1/art/?object_number=' + collectionID + '&output=JSON-LD&lang=en')
-    xhr.send()
+    //Another way of getting an image
+
+    // var xhr = new XMLHttpRequest()
+    // xhr.responseType = 'json';
+    //
+    // xhr.addEventListener('load', () => {
+    //   console.log('getArt ', xhr.response)
+    //   this.setState({
+    //     image: xhr.response.thumbnailUrl
+    //   })
+    // })
+    //
+    // xhr.open('GET', 'https://api.smk.dk/api/v1/art/?object_number=' + collectionID + '&output=JSON-LD&lang=en')
+    // xhr.send()
   }
 
-  getImage = (imgID) => {
-    var xhr = new XMLHttpRequest()
+  getArtInfo = (collectionID) => {
 
-    xhr.addEventListener('load', () => {
-      this.setState({
-        image: xhr.responseURL
-      })
-    })
+    fetch('https://api.smk.dk/api/v1/iiif/manifest/?id=' + collectionID)
+     .then(response => response.json())
+     .then(data => {console.log('img', data); this.setState({ artInfo: Object.values(data.metadata[2].value[0])[0] })});
 
-    // xhr.open('GET', 'https://api.smk.dk/api/v1/thumbnail/' + imgID)
-    xhr.open('GET', this.state.image)
-    xhr.send()
+    //Another way of getting art info
+
+    // var xhr = new XMLHttpRequest()
+    // xhr.responseType = 'json';
+    //
+    // xhr.addEventListener('load', () => {
+    //   console.log('getArt info ', xhr.response)
+    //   this.setState({
+    //     artInfo: Object.values(xhr.response.metadata[2].value[0])[0]
+    //   })
+    // })
+    //
+    // xhr.open('GET', 'https://api.smk.dk/api/v1/iiif/manifest/?id=' + collectionID)
+    // xhr.send()
   }
 
   render() {
     return (
-      <div className="center">
-        <img src={this.state.image} width="400" alt="something" className="thumbnail"/>
+      <div className="center imgContainer">
+        {this.state.image && <img src={this.state.image} width="400" alt="something" className="thumbnail"/>}
+        { this.state.artInfo && <h4>{this.state.artInfo}</h4> }
       </div>
     )
   }
