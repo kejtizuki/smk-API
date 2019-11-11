acf85700d46257c3f84ab46bcc88940c8ea6cdc0
Kasia Zukowska <kzukowska@hotmail.com>
2019-11-11 15:22:15 +0100

    parent chaild example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import Image from './Image/Image'
 
-function App() {
-  return (
-    <div className="App">
-      <Image />
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      collectionIDs: null,
+      slicedIDs: null
+    }
+
+  }
+
+  componentDidMount() {
+    // this.getIDs()
+  }
+
+  // getIDs = () => {
+  //   fetch('https://api.smk.dk/api/v1/art/all_ids')
+  //    .then(response => response.json())
+  //    .then(data => this.setState({ collectionIDs: data.objectIDs }));
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <Image collectionID="KKSgb11925" />
+        <Image collectionID="KMS6630" />
+        <Image collectionID="kks1982-156" />
+      </div>
+    )
+  }
 }
+
 
 export default App;
--- src/Image/Image.js
+++ src/Image/Image.js
@@ -3,74 +3,18 @@ import './image.css'
 
 class Image extends Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    console.log(this.props)
     this.state = {
-      collectionIDs: [],
-      id: 'kks1982-156'
-      // Try other ids, for example 'kks1982-156',
+      image: null,
+      artInfo: null
     }
   }
 
   componentDidMount() {
-    this.getIDs()
-
-    this.getArt(this.state.id)
-    this.getArtInfo(this.state.id)
-  }
-
-  componentDidUpdate(prevState) {
-    if (prevState.collectionIDs !== this.state.collectionIDs) {
-      console.log('print collection IDs ', this.state.collectionIDs)
-    }
-  }
-
-  getIDs = () => {
-
-    fetch('https://api.smk.dk/api/v1/art/all_ids')
-     .then(response => response.json())
-     .then(data => this.setState({ collectionIDs: data.objectIDs }));
-
-
-    //Another way of geting IDs array
-
-    // var xhr = new XMLHttpRequest()
-    // xhr.responseType = 'json';
-    // let jsonResponse
-    // xhr.addEventListener('load', () => {
-    //
-    //   jsonResponse = xhr.response
-    //   console.log(jsonResponse.objectIDs)
-    //   this.setState({
-    //     collectionIDs: jsonResponse.objectIDs
-    //   })
-    // })
-    //
-    // xhr.open('GET', 'https://api.smk.dk/api/v1/art/all_ids')
-    // xhr.send()
-  }
-
-  // Get image using collectionID
-  getArt = (collectionID) => {
-
-    fetch('https://api.smk.dk/api/v1/art/?object_number=' + collectionID + '&output=JSON-LD&lang=en')
-     .then(response => response.json())
-     .then(data => {console.log('dataaa', data); this.setState({ image: data.thumbnailUrl })});
-
-    //Another way of getting an image
-
-    // var xhr = new XMLHttpRequest()
-    // xhr.responseType = 'json';
-    //
-    // xhr.addEventListener('load', () => {
-    //   console.log('getArt ', xhr.response)
-    //   this.setState({
-    //     image: xhr.response.thumbnailUrl
-    //   })
-    // })
-    //
-    // xhr.open('GET', 'https://api.smk.dk/api/v1/art/?object_number=' + collectionID + '&output=JSON-LD&lang=en')
-    // xhr.send()
+    this.getArt(this.props.collectionID)
+    this.getArtInfo(this.props.collectionID)
   }
 
   getArtInfo = (collectionID) => {
@@ -79,20 +23,13 @@ class Image extends Component {
      .then(response => response.json())
      .then(data => {console.log('img', data); this.setState({ artInfo: Object.values(data.metadata[2].value[0])[0] })});
 
-    //Another way of getting art info
+  }
 
-    // var xhr = new XMLHttpRequest()
-    // xhr.responseType = 'json';
-    //
-    // xhr.addEventListener('load', () => {
-    //   console.log('getArt info ', xhr.response)
-    //   this.setState({
-    //     artInfo: Object.values(xhr.response.metadata[2].value[0])[0]
-    //   })
-    // })
-    //
-    // xhr.open('GET', 'https://api.smk.dk/api/v1/iiif/manifest/?id=' + collectionID)
-    // xhr.send()
+  getArt = (collectionID) => {
+
+    fetch('https://api.smk.dk/api/v1/art/?object_number=' + collectionID + '&output=JSON-LD&lang=en')
+     .then(response => response.json())
+     .then(data => {console.log('dataaa', data); this.setState({ image: data.thumbnailUrl })});
   }
 
   render() {
